346eb229c131bf78ce282ee30d977abbed667539
Rohit Bhirud <rbrohitbhirud007@gmail.com>
2017-06-19 17:12:32 +0000

    Admin routes added

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,46 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+/*************************** admin routes ***************************/
+
+/**
+ * Basic home & reports routes
+ */
+
+Route::get('/', 'HomeController@index');
+Route::get('/reports/{type}', 'HomeController@reports');
+
+/**
+ * Engineers resource routes
+ */
+
+Route::get('engineer/details/{id}', 'EngineersController@show');
+Route::get('engineer/complaints', 'EngineersController@complaints');
+
+Route::resource('engineer', 'EngineersController');
+
+/**
+ * User resource routes
+ */
+
+Route::prefix('user')->group(function ()
+{
+	Route::get('/', 'UsersController@index');
+	Route::get('details/{user}', 'UsersController@show');
+	Route::delete('{user}', 'UsersController@destroy');
 });
+
+/**
+ * Complaint resource routes
+ */
+Route::prefix('complaint')->group(function ()
+{
+	Route::get('/{type}', 'ComplaintsController@index');
+	Route::get('/details/{complaint}', 'ComplaintsController@show');
+	Route::get('/{complaint}/edit', 'ComplaintsController@edit');
+	Route::match(['put', 'patch'], '{complaint}', 'ComplaintsController@update');
+	Route::delete('{complaint}', 'ComplaintsController@destroy');
+});
+
+
+/*************************** engineers route ***************************/
