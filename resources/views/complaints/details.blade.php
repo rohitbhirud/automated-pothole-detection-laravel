c5778515653a97767d063f9f8443adbf9c16600d
vagrant <vagrant@homestead>
2017-06-26 10:09:31 +0000

    Mobile number in details page fixed

--- resources/views/complaints/details.blade.php
+++ resources/views/complaints/details.blade.php
@@ -27,7 +27,7 @@
 
   <div class="col-md-8 right-col">
     <div><span class="glyphicon glyphicon-user" aria-hidden="true"></span> {{ $complaint->user->fullname }}</div>
-    <div><span class="glyphicon glyphicon-phone" aria-hidden="true"></span> {{ $complaint->mobile }}</div>
+    <div><span class="glyphicon glyphicon-phone" aria-hidden="true"></span> {{ $complaint->user->mobile }}</div>
     <div><span class="glyphicon glyphicon-alert" aria-hidden="true"></span> {{ $complaint->title }}</div>
     <div><span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span> <span id="longitude">{{ $complaint->longitude }}</span></div>
     <div><span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span> <span id="latitude">{{ $complaint->latitude }}</span></div>
